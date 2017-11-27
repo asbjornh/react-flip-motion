4a097b731e355ea5bdd37a0b856f513707aad46e
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2017-11-27 19:04:28 +0100

    updated jest

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,128 +1,138 @@
-jest
-  .disableAutomock()
-  .useRealTimers()
+jest.disableAutomock().useRealTimers();
 
-const React = require("react")
-const ReactFlipMotion = require("..").default
-const TransitionMotion = require("react-motion").TransitionMotion
-const TestUtils = require("react-addons-test-utils")
+const React = require("react");
+const ReactFlipMotion = require("..").default;
+const TransitionMotion = require("react-motion").TransitionMotion;
+const TestUtils = require("react-dom/test-utils");
 
 describe("ReactFlipMotion", () => {
-
-
   beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000
-  })
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+  });
 
   it("should not error", () => {
-    expect(() => TestUtils.renderIntoDocument(<ReactFlipMotion><div key={1}/><div key={2}/></ReactFlipMotion>))
-      .not.toThrow()
-  })
+    expect(() =>
+      TestUtils.renderIntoDocument(
+        <ReactFlipMotion>
+          <div key={1} />
+          <div key={2} />
+        </ReactFlipMotion>
+      )
+    ).not.toThrow();
+  });
 
   it("should render children", () => {
-
     class View extends React.Component {
       render() {
-        return (
-          <div {...this.props} />
-        )
+        return <div {...this.props} />;
       }
     }
 
     class TestComponent extends React.Component {
       constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-          list: [
-            { id: "0", text: "foo" },
-            { id: "1", text: "bar" },
-          ]
-        }
+          list: [{ id: "0", text: "foo" }, { id: "1", text: "bar" }]
+        };
       }
       render() {
         return (
           <ReactFlipMotion>
-            {this.state.list.map((item) =>
+            {this.state.list.map(item => (
               <View style={{ height: 10, fontSize: 10 }} key={item.id}>
                 {item.text}
               </View>
-            )}
+            ))}
           </ReactFlipMotion>
-        )
+        );
       }
     }
 
-    const testComponent = TestUtils.renderIntoDocument(
-      <TestComponent />
-    )
+    const testComponent = TestUtils.renderIntoDocument(<TestComponent />);
 
-    const flipMotion = TestUtils.findRenderedComponentWithType(testComponent, ReactFlipMotion)
-    const reactMotion = TestUtils.findRenderedComponentWithType(flipMotion, TransitionMotion)
-    const elements = TestUtils.scryRenderedComponentsWithType(reactMotion, View)
-    expect(elements.length).toBe(2)
-
-  })
-
+    const flipMotion = TestUtils.findRenderedComponentWithType(
+      testComponent,
+      ReactFlipMotion
+    );
+    const reactMotion = TestUtils.findRenderedComponentWithType(
+      flipMotion,
+      TransitionMotion
+    );
+    const elements = TestUtils.scryRenderedComponentsWithType(
+      reactMotion,
+      View
+    );
+    expect(elements.length).toBe(2);
+  });
 
   it("should transition between states", () => {
-
     class View extends React.Component {
       render() {
-        return (
-          <div {...this.props} />
-        )
+        return <div {...this.props} />;
       }
     }
 
     class TestComponent extends React.Component {
       constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-          list: [
-            { id: "0", text: "foo" },
-            { id: "1", text: "bar" },
-          ]
-        }
+          list: [{ id: "0", text: "foo" }, { id: "1", text: "bar" }]
+        };
       }
       render() {
         return (
           <ReactFlipMotion>
-            {this.state.list.map((item) =>
+            {this.state.list.map(item => (
               <View style={{ height: 10, fontSize: 10 }} key={item.id}>
                 {item.text}
               </View>
-            )}
+            ))}
           </ReactFlipMotion>
-        )
+        );
       }
     }
 
-    const testComponent = TestUtils.renderIntoDocument(
-      <TestComponent />
-    )
+    const testComponent = TestUtils.renderIntoDocument(<TestComponent />);
 
-    const flipMotion = TestUtils.findRenderedComponentWithType(testComponent, ReactFlipMotion)
-    const reactMotion = TestUtils.findRenderedComponentWithType(flipMotion, TransitionMotion)
-    const elements = TestUtils.scryRenderedComponentsWithType(reactMotion, View)
-    expect(elements.length).toBe(2)
-    expect(elements[0].props.children).toBe("foo")
-    expect(elements[1].props.children).toBe("bar")
+    const flipMotion = TestUtils.findRenderedComponentWithType(
+      testComponent,
+      ReactFlipMotion
+    );
+    const reactMotion = TestUtils.findRenderedComponentWithType(
+      flipMotion,
+      TransitionMotion
+    );
+    const elements = TestUtils.scryRenderedComponentsWithType(
+      reactMotion,
+      View
+    );
+    expect(elements.length).toBe(2);
+    expect(elements[0].props.children).toBe("foo");
+    expect(elements[1].props.children).toBe("bar");
 
     testComponent.setState({
-      list: testComponent.state.list.concat().reverse(),
-    })
+      list: testComponent.state.list.concat().reverse()
+    });
 
-    return new Promise((done) => {
+    return new Promise(done => {
       setTimeout(() => {
-        const flipMotion = TestUtils.findRenderedComponentWithType(testComponent, ReactFlipMotion)
-        const reactMotion = TestUtils.findRenderedComponentWithType(flipMotion, TransitionMotion)
-        const elements = TestUtils.scryRenderedComponentsWithType(reactMotion, View)
-        expect(elements.length).toBe(2)
-        expect(elements[0].props.children).toBe("bar")
-        expect(elements[1].props.children).toBe("foo")
-        done()
-      }, 50)
-    })
-  })
-
-})
+        const flipMotion = TestUtils.findRenderedComponentWithType(
+          testComponent,
+          ReactFlipMotion
+        );
+        const reactMotion = TestUtils.findRenderedComponentWithType(
+          flipMotion,
+          TransitionMotion
+        );
+        const elements = TestUtils.scryRenderedComponentsWithType(
+          reactMotion,
+          View
+        );
+        expect(elements.length).toBe(2);
+        expect(elements[0].props.children).toBe("bar");
+        expect(elements[1].props.children).toBe("foo");
+        done();
+      }, 50);
+    });
+  });
+});
